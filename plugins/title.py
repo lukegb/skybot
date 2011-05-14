2013b2be1bc05620b135888957135170d7c4f851
303 <Michael.Schuster.MS@gmail.com>
2011-05-14 10:18:21 +0200

    title now interprets html entity defs, added .title ^ to get title of last url in history. fix for getting title of nonhtml urls

--- plugins/title.py
+++ plugins/title.py
@@ -1,6 +1,8 @@
 from util import hook, http
+import urlhistory
+import htmlentitydefs
 import re
-titler = re.compile(r'<title>(.+?)</title>')
+titler = re.compile(r'(?i)<title>(.+?)</title>')
 
 @hook.regex(r".*(minecraftforum\.net/viewtopic\.php\?[\w=&]+)\b.*")
 def regex(inp, say=None):
@@ -10,8 +12,24 @@ def regex(inp, say=None):
         return
     say("mcforum title: "+t)
 
+
 @hook.command
 @hook.command("t")
-def title(inp):
+def title(inp, db=None):
     ".title <url> - get title of <url>"
-    return titler.search(http.get(inp)).group(1)
+    if inp == '^':
+        urlhistory.db_init(db)
+        rows = db.execute("select url from urlhistory order by time desc limit 1")
+        if not rows.rowcount:
+            return "No url in history."
+        inp = rows.fetchone()[0]
+
+    match = titler.search(http.get(inp))
+    if not match:
+        return "Error: no title"
+
+    rawtitle = match.group(1)
+
+    title = re.sub("&("+"|".join(htmlentitydefs.entitydefs.keys())+");",
+                   lambda m: unichr(htmlentitydefs.name2codepoint[m.group(1)]), rawtitle)
+    return title
