7959fb7c970414dbf54cac9ede37250a0f1e22cd
303 <Michael.Schuster.MS@gmail.com>
2011-05-14 15:29:38 +0200

    added .repaste, .deadfly, .isgd, .expand

---
+++ plugins/repaste.py
@@ -0,0 +1,104 @@
+from util import hook, http
+
+import urllib
+import mechanize
+import urllib2
+import htmlentitydefs
+import re
+
+def unescape_html(text):
+    return re.sub("&("+"|".join(htmlentitydefs.entitydefs.keys())+");",
+                  lambda m: unichr(htmlentitydefs.name2codepoint[m.group(1)]), text)
+
+def scrape_mibpaste(url):
+    pagesource = http.get(url)
+    rawpaste = re.search(r'(?s)(?<=<body>\n).+(?=<hr>)', pagesource).group(0)
+    filterbr = rawpaste.replace("<br />", "")
+    unescaped = unescape_html(filterbr)
+    stripped = unescaped.strip()
+
+    return stripped
+
+def paste_sprunge(text, syntax=None, user=None):
+    data = urllib.urlencode({"sprunge":text})
+    url = urllib2.urlopen("http://sprunge.us/", data).read().strip()
+
+    if syntax:
+        url += "?"+syntax
+
+    return url
+
+def paste_ubuntu(text, user=None, syntax='text'):
+    data = urllib.urlencode({"poster": user,
+                             "syntax": syntax,
+                             "content":text})
+
+    return urllib2.urlopen("http://paste.ubuntu.com/", data).url
+
+def paste_gist(text, user=None, syntax="", description=""):
+    br = mechanize.Browser()
+    br.open('https://gist.github.com')
+
+    br.select_form(nr=1)
+    br.form.find_control('file_contents[gistfile1]').value = text
+
+    if syntax:
+        br.form.find_control('file_name[gistfile1]').value = "repaste."+syntax
+    if description:
+        br.form.find_control('description').value = description
+    br.submit(nr=0)
+
+    return br.geturl()
+
+def paste_strictfp(text, user=None, syntax="plain"):
+    data = urllib.urlencode(dict(
+        language = syntax,
+        paste = text,
+        private = "private",
+        submit="Paste"))
+    req = urllib2.urlopen("http://paste.strictfp.com/", data)
+    return req.url
+
+pasters = dict(
+    ubuntu=paste_ubuntu,
+    sprunge=paste_sprunge,
+    gist=paste_gist,
+    strictfp=paste_strictfp
+)
+
+@hook.command
+def repaste(inp, user=None):
+    ".repaste list|[provider] [syntax] <mibpasteurl> -- scrape mibpaste, reupload on given pastebin"
+
+    parts = inp.split()
+
+    if parts[0] == 'list':
+        return " ".join(sorted(pasters.keys()))
+
+    paster = paste_ubuntu
+    args = {}
+
+    if not parts[0].startswith("http"):
+        p = parts[0].lower()
+
+        if p in pasters:
+            paster = pasters[p]
+            parts = parts[1:]
+
+    if not parts[0].startswith("http"):
+        p = parts[0].lower()
+        parts = parts[1:]
+
+        args["syntax"] = p
+
+    if len(parts) > 1:
+        return "PEBKAC"
+
+    args["user"] = user
+    
+    args["text"] = scrape_mibpaste(parts[0])
+    pasted = paster(**args)
+
+    return pasted
+
+
---
+++ plugins/shorten.py
@@ -0,0 +1,53 @@
+from util import hook, http
+import urllib
+import urllib2
+import httplib
+import time
+import urlparse
+import re
+
+re_adfly = re.compile(r'var url = \'([^\']+)\'')
+
+@hook.command
+def isgd(inp):
+    ".isgd <url> -- shorten link using is.gd"
+
+    data = urllib.urlencode(dict(format="simple",url=inp))
+
+    shortened = urllib2.urlopen("http://is.gd/create.php", data).read()
+
+    return shortened
+
+@hook.command
+def expand(inp):
+    ".expand <shorturl> -- expand link shortened url"
+
+    # try HEAD
+    parts = urlparse.urlsplit(inp)
+    conn = httplib.HTTPConnection(parts.hostname)
+    conn.request('HEAD',parts.path)
+    resp = conn.getresponse()
+    location = resp.msg.getheader("Location")
+
+    if not location:
+        return inp
+
+    expandedparts = urlparse.urlsplit(location)
+    quoted = expandedparts._replace(path=urllib.quote(expandedparts.path))
+    url = quoted.geturl()
+
+    return url
+
+@hook.command
+def deadfly(inp):
+    ".deadfly <adf.ly url> -- scrapes link shortened by adf.ly"
+    parts = urlparse.urlsplit(inp)
+    parts = parts._replace(netloc=parts.netloc + ".nyud.net")
+    url = urlparse.urlunsplit(parts)
+
+    try:
+        text = urllib2.urlopen(url, timeout=10).read()
+    except URLError:
+        return "Timeout"
+
+    return re_adfly.search(text).group(1)
