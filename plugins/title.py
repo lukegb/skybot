343c92f4b81ab5d7af0a04c698316a31eb62a697
303 <Michael.Schuster.MS@gmail.com>
2011-05-15 14:20:14 +0200

    .title checks content type first using a HEAD request, better error reporting

--- plugins/title.py
+++ plugins/title.py
@@ -1,8 +1,12 @@
 from util import hook, http
 import urlhistory
+import httplib
 import re
+import urlparse
+import urllib2
 import repaste
 
+maxlen = 4086
 titler = re.compile(r'(?si)<title>(.+?)</title>')
 
 @hook.regex(r".*(minecraftforum\.net/viewtopic\.php\?[\w=&]+)\b.*")
@@ -12,6 +16,24 @@ def regex(inp, say=None):
     if t=="Login":
         return
     say("mcforum title: "+t)
+
+def check_response(headers):
+    type = headers.get("content-type", None)
+
+    if not type or "html" not in type:
+        reply = "Link is not HTML but %s" % type
+
+        length = headers.get("content-length", None)
+
+        if length is not None:
+            reply += ", length %s" % length
+
+        lastmodified = headers.get("last-modified", None)
+
+        if lastmodified is not None:
+            reply += ", last modified %s " % lastmodified
+
+        return reply
 
 @hook.command
 @hook.command("t")
@@ -25,7 +47,42 @@ def title(inp, db=None, chan=None):
 
         inp = rows.fetchone()[0]
 
-    match = titler.search(http.get(inp))
+    try:
+        parts = urlparse.urlsplit(inp)
+        conn = httplib.HTTPConnection(parts.hostname, timeout=10)
+        path = parts.path
+
+        if parts.query:
+            path += "?"+parts.query
+
+        conn.request('HEAD',path)
+        resp = conn.getresponse()
+
+        if not (200 <= resp.status < 400):
+            return "Error: HEAD %s %s" % (resp.status, resp.reason)
+
+        errors = check_response(dict(resp.getheaders()))
+
+        if errors:
+            return errors
+    except Exception as e:
+        return "Error: " + str(e)
+
+    try:
+        req = urllib2.urlopen(inp)
+    except urllib2.HTTPError as e:
+        return "Error: GET %s %s" % (e.code, e.reason)
+    except urllib2.URLError as e:
+        return "Error: GET %s" % e.reason
+
+    errors = check_response(req.headers)
+
+    if errors:
+        return errors
+
+    text = req.read(maxlen).decode('utf8', 'ignore')
+
+    match = titler.search(text)
 
     if not match:
         return "Error: no title"
