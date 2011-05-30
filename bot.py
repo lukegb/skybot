24d94529133e48145c8ee26422fd261194004abd
xales <xales@stratos>
2011-05-30 04:24:16 -0400

    Complete overhaul of IRC and the main loop - now uses select()

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import os
 import Queue
 import sys
 import time
+import select
 
 sys.path += ['plugins']  # so 'import hook' works without duplication
 sys.path += ['lib']
@@ -34,13 +35,14 @@ bot.conns = {}
 
 try:
     for name, conf in bot.config['connections'].iteritems():
-        if conf.get('ssl'):
-            bot.conns[name] = SSLIRC(conf['server'], conf['nick'], conf=conf,
-                    port=conf.get('port', 6667), channels=conf['channels'],
-                    ignore_certificate_errors=conf.get('ignore_cert', True))
-        else:
-            bot.conns[name] = IRC(conf['server'], conf['nick'], conf=conf,
-                    port=conf.get('port', 6667), channels=conf['channels'])
+        bot.conns[name] = IRC(main, conf['server'], conf['nick'], conf=conf, port=conf.get('port', 6667), channels=conf['channels'])
+#        if conf.get('ssl'):
+#            bot.conns[name] = SSLIRC(conf['server'], conf['nick'], conf=conf,
+#                    port=conf.get('port', 6667), channels=conf['channels'],
+#                    ignore_certificate_errors=conf.get('ignore_cert', True))
+#        else:
+#            bot.conns[name] = IRC(conf['server'], conf['nick'], conf=conf,
+#                    port=conf.get('port', 6667), channels=conf['channels'])
 except Exception, e:
     print 'ERROR: malformed config file', e
     sys.exit()
@@ -50,16 +52,31 @@ if not os.path.exists(bot.persist_dir):
     os.mkdir(bot.persist_dir)
 
 print 'Running main loop'
-
+socks = [s[1].conn.socket for s in bot.conns.iteritems()]
+sockmap = {}
+for conn in bot.conns.iteritems():
+    sockmap[conn[1].conn.socket.fileno()] = conn[1]
 while True:
-    reload()  # these functions only do things
-    config()  # if changes have occured
+    reload()
+    config()
+    r, w, x = select.select(socks, [], [], 0.2)
+    for c in r:
+        if not sockmap[c.fileno()].loop_read():
+            socks.remove(c)
+            if len(socks) == 0:
+                break
+    for c in socks:
+        sockmap[c.fileno()].loop_write()
 
-    for conn in bot.conns.itervalues():
-        try:
-            out = conn.out.get_nowait()
-            main(conn, out)
-        except Queue.Empty:
-            pass
-    while all(conn.out.empty() for conn in bot.conns.itervalues()):
-        time.sleep(.1)
+#while True:
+#    reload()  # these functions only do things
+#    config()  # if changes have occured
+#
+#    for conn in bot.conns.itervalues():
+#        try:
+#            out = conn.out.get_nowait()
+#            main(conn, out)
+#        except Queue.Empty:
+#            pass
+#    while all(conn.out.empty() for conn in bot.conns.itervalues()):
+#        time.sleep(.1)
