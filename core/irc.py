71b3723caa35ff143196b83247badd04dcc52700
xales <xales@stratos>
2011-05-30 06:45:28 -0400

    Fixed trailing \r\n

--- core/irc.py
+++ core/irc.py
@@ -81,6 +81,7 @@ class IRC(object):
         self.cmd('USER', [conf.get('user', 'skybot'), '3', '*', conf.get('realname', 'skybot 2.0')])
     def process(self, msg):
         msg = decode(msg)
+        msg = msg.replace("\r\n", "")
         if msg.startswith(':'):
             prefix, command, params = irc_prefix_rem(msg).groups()
         else:
