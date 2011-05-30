8c0e90ba59af272da314de2173d664b3c4ddde42
lahwran <blendmaster1024@gmail.com>
2011-05-30 00:26:41 -0600

    attempt to fix forever-lock error

--- plugins/usertracking.py
+++ plugins/usertracking.py
@@ -139,70 +139,78 @@ class Userdict(dict):
 @hook.sieve
 def valueadd(bot, input, func, kind, args):
     global loaded
-    global userlock
     if not userlock.acquire(): raise Exception("Problem acquiring userlock, probable thread crash. Abort.")
-    if not hasattr(input.conn, "users") or not loaded:
-        loaded = True
-        input.conn.users = Users()
-        input.conn.users.users[input.nick] = User(input.nick, input.nick, "127.0.0.1")
-    input["users"] = input.conn.users
-    input["userdata"] = input.conn.users._user(input.nick, input.user, input.host)
-    if input.chan in input.conn.users.channels:
-        input["chandata"] = input.conn.users[input.chan]
-    else:
-        input["chandata"] = None
-    botmodes.valueadd(bot, input, func, kind, args)
-    userlock.release()
-    return input
+    try:
+        if not hasattr(input.conn, "users") or not loaded:
+            loaded = True
+            input.conn.users = Users()
+            input.conn.users.users[input.nick] = User(input.nick, input.nick, "127.0.0.1")
+        input["users"] = input.conn.users
+        input["userdata"] = input.conn.users._user(input.nick, input.user, input.host)
+        if input.chan in input.conn.users.channels:
+            input["chandata"] = input.conn.users[input.chan]
+        else:
+            input["chandata"] = None
+        botmodes.valueadd(bot, input, func, kind, args)
+        userlock.release()
+        return input
+    except:
+        raise
+    finally:
+        userlock.release()
 
 
 @hook.event("332 353 311 319 312 330 318 JOIN PART KICK QUIT PRIVMSG MODE NICK")
 @hook.singlethread
 def tracking(inp, command=None, input=None, users=None):
     if not userlock.acquire(): raise Exception("Problem acquiring userlock, probable thread crash. Abort.")
-    if command in ["JOIN", "PART", "KICK", "QUIT", "PRIVMSG", "MODE", "NICK"]:
-        if input.nick != input.conn.nick and input.chan.startswith("#") and input.chan not in users.channels:
-            input.conn.send("NAMES " + input.chan)
-            users._chan(input.chan)
-    if command == "353":  # when the names list comes in
-        chan = inp[2]
-        names = inp[3]
-        for name in names.split(" "):
-            match = flag_re.match(name)
-            flags = match.group(1)
-            nick = match.group(2)
-            users._join(nick, None, None, chan, flags)
-    elif command == "311":  # whois: nick, user, host, realname"
-        nick = inp[1]
-        user = inp[2]
-        host = inp[3]
-        if nick not in input.conn.users.users.keys():
-            users._user(nick, user, host)
-        users[nick].realname = inp[5]
-    elif command == "319":  # whois: channel list
-        users[inp[1]].channels = inp[2].split(" ")
-    elif command == "312":  # whois: server
-        users[inp[1]].server = inp[2]
-    elif command == "330":  # whois: user logged in
-        print inp
-        users[inp[1]].authed = inp[2]
-    elif command == "318":  # whois: end of whois
-        user = users[inp[1]]
-        user.authed = user.authed or ""
-    elif command == "JOIN":
-        users._join(input.nick, input.user, input.host, input.chan)
-    elif command in ["PART", "KICK", "QUIT"]:
-        for channel in users.channels.values():
-            if input.nick in channel.users:
-                users._exit(input.nick, channel.name)
-        users._trydelete(input.nick)
-    elif command == "PRIVMSG":  # updates last seen time - different from seen plugin
-        users[input.nick].lastmsg = time.time()
-    elif command == "MODE":  # mode changes - getting op and suchh
-        users._mode(*inp)
-    elif command == "NICK":
-        users._chnick(input.nick, inp[0])
-    userlock.release()
+    try:
+        if command in ["JOIN", "PART", "KICK", "QUIT", "PRIVMSG", "MODE", "NICK"]:
+            if input.nick != input.conn.nick and input.chan.startswith("#") and input.chan not in users.channels:
+                input.conn.send("NAMES " + input.chan)
+                users._chan(input.chan)
+        if command == "353":  # when the names list comes in
+            chan = inp[2]
+            names = inp[3]
+            for name in names.split(" "):
+                match = flag_re.match(name)
+                flags = match.group(1)
+                nick = match.group(2)
+                users._join(nick, None, None, chan, flags)
+        elif command == "311":  # whois: nick, user, host, realname"
+            nick = inp[1]
+            user = inp[2]
+            host = inp[3]
+            if nick not in input.conn.users.users.keys():
+                users._user(nick, user, host)
+            users[nick].realname = inp[5]
+        elif command == "319":  # whois: channel list
+            users[inp[1]].channels = inp[2].split(" ")
+        elif command == "312":  # whois: server
+            users[inp[1]].server = inp[2]
+        elif command == "330":  # whois: user logged in
+            print inp
+            users[inp[1]].authed = inp[2]
+        elif command == "318":  # whois: end of whois
+            user = users[inp[1]]
+            user.authed = user.authed or ""
+        elif command == "JOIN":
+            users._join(input.nick, input.user, input.host, input.chan)
+        elif command in ["PART", "KICK", "QUIT"]:
+            for channel in users.channels.values():
+                if input.nick in channel.users:
+                    users._exit(input.nick, channel.name)
+            users._trydelete(input.nick)
+        elif command == "PRIVMSG":  # updates last seen time - different from seen plugin
+            users[input.nick].lastmsg = time.time()
+        elif command == "MODE":  # mode changes - getting op and suchh
+            users._mode(*inp)
+        elif command == "NICK":
+            users._chnick(input.nick, inp[0])
+    except:
+        raise
+    finally:
+        userlock.release()
 
 
 @hook.command
